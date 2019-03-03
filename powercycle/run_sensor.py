8bafedebc912ee8464c6ee47d720349c01ad0130
nep67 <nickpipion67@gmail.com>
2019-03-03 20:33:06 +0000

    Pull operations into functions
    Created functions for the sensor reading list and the time measuring
    list. Originally they were just in loops. Also created a functions that
    writes the time measure list to a file. Currently the file saves to
    working directory. Will change to save to a sensordata file.

--- powercycle/run_sensor.py
+++ powercycle/run_sensor.py
@@ -7,23 +7,40 @@ GPIO.setup(17, GPIO.IN)
 GPIO.add_event_detect(17, GPIO.FALLING)
 
 values = []
+time_values = []
 
-i = 0;
-while i < 10:
-    if GPIO.event_detected(17):
-        values.append(time.time())
-        i+=1
+def raw_input():
+    i = 0;
+    while i < 10:
+        if GPIO.event_detected(17):
+            values.append(time.time())
+            i+=1
+    return values
+
+def edge_measure():
+    for index, item in enumerate(values, start=0):
+        time_values.append(values[index +1] - values[index])
+        if index == 8:
+            break
+    return time_values
+
+def textwrite():
+    outfile = open("myOutFile.txt", "w")
+    for line in time_values:
+        outfile.write(str(line))
+        outfile.write("\n")
+    outfile.close()
+
+raw_input()
 
 print('Sensor time values: ',values)
 
-time_values = []
-
-for index, item in enumerate(values, start=0):
-    time_values.append(values[index +1] - values[index])
-    if index == 8:
-        break
-
+edge_measure()
 
 print(time_values)
+
+textwrite()
+
+print('File created')
 
 
