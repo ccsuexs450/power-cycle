fd798916ce6771db31d82d1593f09fbb52bb5478
EthanButtery <ethanbuttery@gmail.com>
2019-03-03 23:20:29 -0500

    Changes frame instead of creating new window

--- GUI.py
+++ GUI.py
@@ -1,58 +1,69 @@
+import tkinter as tk
 from tkinter import *
 import sys
 import os
 
-def runScript():
-    os.system('python Script.py')
-def calWindow():
-    window1 = Toplevel()
-    window1.configure(bg="dim gray")
-    window1.geometry("800x800")
+class GUI(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
 
-    menu = Menu(window)
-    window1.config(menu=menu)
+        container = tk.Frame(self)
+        container.pack()
+        self.geometry("800x800")
 
-    subMenu = Menu(menu, tearoff=0)
-    menu.add_cascade(label="File", menu=subMenu)
-    subMenu.add_command(label="Exit", command=window1.quit)
+        menu = Menu(self)
+        self.config(menu=menu)
+        subMenu = Menu(menu, tearoff=0)
+        menu.add_cascade(label="File", menu=subMenu)
+        subMenu.add_command(label="Exit", command=self.quit)
 
-    title = Label(window1, text="Calibration", font=("Courier", 44), fg="white", bg="dim gray")
-    title.grid(row=0, columnspan=150)
-    calibrateButton = Button(window1, text="Run Calibration", height=3, width=18, command=runScript)
-    calibrateButton.grid(row=25, column=72, padx=2, pady=2)
-    col_count, row_count = window1.grid_size()
+        self.frames = {}
+        for F in (Home, Calibrate):
+            page = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
 
+        self.show("Home")
 
-    for col in range(col_count):
-        window1.grid_columnconfigure(col, minsize=10)
+    def show(self, page):
+        frame = self.frames[page]
+        frame.tkraise()
 
-    for row in range(row_count):
-        window1.grid_rowconfigure(row, minsize=10)
+class Home(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        title = tk.Label(self, text="Bicycle Application", font=("Courier", 44), fg="black",)
+        title.grid(row=1, column=1)
+        calibrateButton = tk.Button(self, text="Calibrate", height=2, width=10, command=lambda: controller.show("Calibrate"))
+        calibrateButton.grid(row=2, column=1, padx=2, pady=2)
+        runButton = tk.Button(self, text="Run", height=4, width=20, command=lambda: controller.show("Calibrate"))
+        runButton.grid(row=3, column=1, padx=2, pady=2)
 
-window = Tk()
-window.configure(bg="dim gray")
-window.geometry("800x800")
+        col_count, row_count = self.grid_size()
+        for col in range(col_count):
+            self.grid_columnconfigure(col, minsize=10)
+        for row in range(row_count):
+            self.grid_rowconfigure(row, minsize=10)
 
-menu = Menu(window)
-window.config(menu=menu)
+class Calibrate(tk.Frame):
+    def __init__(self, parent, controller):
+        def runScript():
+            os.system('python Script.py')
+        tk.Frame.__init__(self, parent)
+        title = tk.Label(self, text="Calibration", font=("Courier", 44), fg="black")
+        title.grid(row=1, column=15)
+        calibrateButton = tk.Button(self, text="Run Calibration", height=4, width=24, command=runScript)
+        calibrateButton.grid(row=2, column=15, padx=2, pady=2)
+        homeButton = tk.Button(self, text="Home", height=3, width=18, command=lambda: controller.show("Home"))
+        homeButton.grid(row=3, column=15, padx=2, pady=2)
 
-subMenu = Menu(menu, tearoff=0)
-menu.add_cascade(label="File", menu=subMenu)
-subMenu.add_command(label="Exit", command=window.quit)
+        col_count, row_count = self.grid_size()
+        for col in range(col_count):
+            self.grid_columnconfigure(col, minsize=10)
+        for row in range(row_count):
+            self.grid_rowconfigure(row, minsize=10)
 
-title = Label(window, text="Bicycle Application", font=("Courier", 44), fg="white", bg="dim gray")
-title.grid(row=0, columnspan=150)
-insertButton = Button(window, text="Calibrate", height=2, width=10, command=calWindow)
-insertButton.grid(row=20, column=72, padx=2, pady=2)
-printButton = Button(window, text="Run", height=4, width=20, command=runScript)
-printButton.grid(row=25, column=72, padx=2, pady=2)
-
-col_count, row_count = window.grid_size()
-
-for col in range(col_count):
-    window.grid_columnconfigure(col, minsize=10)
-
-for row in range(row_count):
-    window.grid_rowconfigure(row, minsize=10)
-
-window.mainloop()
+if __name__=="__main__":
+    gui = GUI()
+    gui.mainloop()
