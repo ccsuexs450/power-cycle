e38cb53c9611d07d4c0bbd0a7c35d4337dc8f1d4
nep67 <nickpipino67@gmail.com>
2019-03-25 23:03:52 +0000

    Create seperate functions for calibration and run scenarios
    Made changes to run_sensor.py to accomadate both scenarios. Added a test
    function for running the calibration. It currently reads test input
    from file.

--- powercycle/run_sensor.py
+++ powercycle/run_sensor.py
@@ -4,10 +4,10 @@ import io
 import time
 from db_interaction import *
 
-valuevalues = []
+values = []
 time_values = []
 
-def running(user_email)
+def sensor_running(user_email):
     
     ser=serial.Serial("/dev/ttyACM0",9600)  #change ACM number as found from ls /de$
     ser.baudrate=9600
@@ -18,7 +18,18 @@ def running(user_email)
     textwrite(user_email)
     print('File created')
 
-def sensor_input():
+
+def calibrate_input(): #placeholder until sensor is working. Reads test input from file.
+
+    with open("../data/sensordata/calibrate.txt", "r") as ins:
+    for line in ins:
+        line = line.rstrip('\n')
+        values.append(line)
+
+    return values
+    
+
+def performance_input():
     i = 0
     while i < 30:
 
