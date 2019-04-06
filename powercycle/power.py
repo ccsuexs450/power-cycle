689cc203c83a08131582eeda52cc68fdfa332e27
nep67 <nickpipino67@gmail.com>
2019-04-06 19:36:44 +0000

    Power sheet manipulation
    Created a python script to manipulate the power spreadsheet template.

--- powercycle/power.py
+++ powercycle/power.py
@@ -7,39 +7,41 @@ from db_interaction import *
 soffice = subprocess.Popen([
     'lxterminal',
     '-e',
-    '/usr/bin/soffice',
+    'soffice',
     '--accept=host=localhost,port=2002;urp;',
     '--norestore',
     '--nologo',
     '--nodefault',
     '--headless'])
 
-
 lines = []
+dt    = []
 
 desktop = pyoo.Desktop('localhost', 2002)
 doc = desktop.open_spreadsheet("../docs/templates/Calibrate_blank.ods")
 
-sheet = doc.sheets[0]
+power = doc.sheets[1]
+delta = doc.sheets[2]
 
-with open("../data/sensordata/calibrate.txt", "r") as ins:
+with open("../data/sensordata/power.txt", "r") as ins:
     for line in ins:
         line = line.rstrip('\n')
         lines.append(line)
 
 print(lines[0:10])
  
-sheet[1:796,0].values = lines
+power[1:496,0].values = lines
 
-delta_theta = sheet[1:15,10].values
+with open("../docs/templates/delta_theta.txt", "r") as ins:
+    for line in ins:
+        line = line.rstrip('\n')
+        dt.append(line)
 
-with open("../docs/templates/delta_theta.txt", "w") as out:
-    for item in delta_theta:
-        out.write("%s\n" % item)
+delta[1:16,10].values = dt
 
-path = "../docs/calibration/"
-date = "date_time" # place holder
-filename = "calibrate_now_date_time.ods"  # place holder
+path = "../docs/power/"
+date = time.strftime("%Y%m%d-%H%M%S")
+filename = "username" + date + ".ods"
 file_path = path + filename
 doc.save(file_path)
 calibrate_insert(filename, file_path, date)
@@ -48,4 +50,3 @@ doc.close()
 soffice.kill()
 
 print("File Saved")
-
