884d50f01484795573680e1ac6c8efecc5f54785
nep67 <nickpipino67@gmail.com>
2019-04-24 16:26:30 +0000

    Graph finished
    The graphing function is finally done. The first value in the torque
    array is ommitted when building the graph. This is the only way I was
    able to get the graph to look like the one in the spreadsheet. Also the
    torque y axis might need to be adjusted later.

--- powercycle/power_chart.py
+++ powercycle/power_chart.py
@@ -18,24 +18,28 @@ def draw_graph(datax, datay1, datay2, email):
     t = np.linspace(x_sm.min(), x_sm.max(), 100)
     
     color = 'k'
-    ax1.set_xlim([50, 200])
-    ax1.set_ylim([0, 1400])
+    ax1.set_xlim([50, x_sm.max()+20])
+    ax1.set_ylim([0, y1_sm.max() + 200])
     ax1.set_xlabel('Pedaling Rate(rpm)')
     ax1.set_ylabel('Power(watts)', color=color)
-    ax1.plot(x_sm, y1_sm, '.', t, p(t),color=color, linewidth = 4)
+    ax1.plot( t, p(t),color=color, linewidth = 3)
     ax1.tick_params(axis='y', labelcolor=color)
+    
+    x2len = len(datax)
+    y2len = len(datay2)
 
-    y2_sm = np.array(datay2)
-    z = np.polyfit(x_sm, y2_sm, 1)
+    x2_sm = np.array(datax[1:x2len -1])
+    y2_sm = np.array(datay2[1:y2len-1])
+    z = np.polyfit(x2_sm, y2_sm, 1)
     p = np.poly1d(z)
     
     ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-    ax2.set_xlim([50, 200])
-    ax2.set_ylim([0,140])
+    ax2.set_xlim([50, x_sm.max() + 20])
+    ax2.set_ylim([0, 140])
     color = 'tab:red'
     ax2.set_ylabel('Torque(Nm)', color=color)  # we already handled the x-label with ax1
-    ax2.plot(x_sm, y2_sm, '.', color=color)
-    ax2.plot(x_sm, p(x_sm), '-',  color=color, linewidth = 4)
+   # ax2.plot(x_sm, y2_sm, '.', color=color)
+    ax2.plot(x2_sm, p(x2_sm), '-',  color=color, linewidth = 4)
     
 
     ax2.tick_params(axis='y', labelcolor=color)
